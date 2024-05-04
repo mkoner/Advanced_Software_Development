7019ce2df1fbdac96119070b5a80fb13258ee8bd
mkoner <amzid@hotmail.fr>
2024-05-04 15:18:44 -0500

    Lab4 init

---
+++ Lab4-Factory/Lab4/src/main/java/edu/mum/cs/cs525/labs/skeleton/service/AccountServiceImpl.java
@@ -0,0 +1,58 @@
+package edu.mum.cs.cs525.labs.skeleton.service;
+
+import edu.mum.cs.cs525.labs.skeleton.domain.Customer;
+import edu.mum.cs.cs525.labs.skeleton.domain.Account;
+import edu.mum.cs.cs525.labs.skeleton.repository.AccountDAO;
+import edu.mum.cs.cs525.labs.skeleton.repository.AccountDAOImpl;
+
+import java.util.Collection;
+
+public class AccountServiceImpl implements AccountService {
+	private AccountDAO accountDAO;
+	
+	public AccountServiceImpl(){
+		accountDAO = new AccountDAOImpl();
+	}
+
+	public Account createAccount(String accountNumber, String customerName) {
+		Account account = new Account(accountNumber);
+		Customer customer = new Customer(customerName);
+		account.setCustomer(customer);
+		
+		accountDAO.saveAccount(account);
+		
+		return account;
+	}
+
+	public void deposit(String accountNumber, double amount) {
+		Account account = accountDAO.loadAccount(accountNumber);
+		account.deposit(amount);
+		
+		accountDAO.updateAccount(account);
+	}
+
+	public Account getAccount(String accountNumber) {
+		Account account = accountDAO.loadAccount(accountNumber);
+		return account;
+	}
+
+	public Collection<Account> getAllAccounts() {
+		return accountDAO.getAccounts();
+	}
+
+	public void withdraw(String accountNumber, double amount) {
+		Account account = accountDAO.loadAccount(accountNumber);
+		account.withdraw(amount);
+		accountDAO.updateAccount(account);
+	}
+
+
+
+	public void transferFunds(String fromAccountNumber, String toAccountNumber, double amount, String description) {
+		Account fromAccount = accountDAO.loadAccount(fromAccountNumber);
+		Account toAccount = accountDAO.loadAccount(toAccountNumber);
+		fromAccount.transferFunds(toAccount, amount, description);
+		accountDAO.updateAccount(fromAccount);
+		accountDAO.updateAccount(toAccount);
+	}
+}
